d59c64819a545f330dd2333dca667237c0f9d314
ktanonymous <kota.shoji.0725@gmail.com>
2022-08-24 23:45:07 +0900

    `create_dummy` function accept multiple parameter input `params`.

--- src/dummy_creator/main.py
+++ src/dummy_creator/main.py
@@ -1,27 +1,25 @@
 import json
 import os
 
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from .core import create_data
 
 
-def create_dummy(input_files: Optional[List[str]] = None, **kwargs):
-    if input_files is None:
-        try:
-            parameters = dict(kwargs['parameters'])
-        except KeyError:
-            print('Please specify parameter of json file representting parameter.')
-            return
-        return create_data(parameters)
-
-    assert issubclass(type(input_files), list), 'assertion!!!'
-
+def create_dummy(params: List[Dict[str, str]],
+                 input_files: Optional[List[str]] = None):
     data: dict = {}
-    for i, input_file in enumerate(input_files):
-        assert os.path.isfile(input_file)
-        with open(input_file, 'r') as f:
-            parameters = json.load(f)
-        data[i] = create_data(parameters)
+
+    if input_files is None:
+        assert issubclass(type(params), list)
+        for i, param in enumerate(params):
+            data[i] = create_data(param)
+    else:
+        assert issubclass(type(input_files), list)
+        for i, input_file in enumerate(input_files):
+            assert os.path.isfile(input_file)
+            with open(input_file, 'r') as f:
+                parameters = json.load(f)
+            data[i] = create_data(parameters)
 
     return data
